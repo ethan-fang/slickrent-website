10a28d975053bf79131e71ac3661d0051cab358c
xinxin <xinxin.wang.1@gmail.com>
2015-02-02 21:12:14 -0800

    add time picker

--- webapp/js/borrow.js
+++ webapp/js/borrow.js
@@ -1,46 +1,28 @@
 var borrowApp = angular.module('borrow', []);
 
 
+var SERVICE_HOST = "http://localhost:8080/"
+//var SERVICE_HOST = "https://ec2-54-173-114-114.compute-1.amazonaws.com/"
+
+
 borrowApp.controller('borrowController', ['$scope', '$log', '$http', function($scope, $log, $http){
 
-    this.items = [
-        {name: 'abc', description: 'description', price: '$5'},
-        {name: 'abc', description: 'description', price: '$5'},
-        {name: 'abc', description: 'description', price: '$5'},
-        {name: 'abc', description: 'description', price: '$5'},
-        {name: 'abc', description: 'description', price: '$5'}
+    $scope.items = [
+//        {name: 'abc', description: 'description', price: '$5'},
+//        {name: 'abc', description: 'description', price: '$5'},
+//        {name: 'abc', description: 'description', price: '$5'},
+//        {name: 'abc', description: 'description', price: '$5'},
+//        {name: 'abc', description: 'description', price: '$5'}
     ];
 
 
 
+    $http.get(SERVICE_HOST + "shareitem?clientId=e7568b2c-2c0f-480e-9e34-08f9a4b807dc&size=5")
+         .success(function (response) {
 
-    var fetchItems = function() {
-        $log.info("fetching data");
-        $http.get("https://ec2-54-173-114-114.compute-1.amazonaws.com/shareitem?clientId=e7568b2c-2c0f-480e-9e34-08f9a4b807dc&size=2")
-            .success(function (data) {
-//                this.items = data;
-                $log.info(data);
-             });
-    };
-
-    fetchItems($http);
-
+        $scope.items = response.items;
+        $log.info(response);
+     });
 }]);
-
-
-//app.service('feedbackService', function ($http) {
-//    this.getFeedbackPaged = function (nodeId, pageNumber, take) {
-//        return $http.get('myUrl');
-//    };
-//});
-//
-//app.controller('feedbackController', function ($scope, feedbackService, $filter) {
-//// Constructor for this controller
-//    init();
-//
-//    function init() {
-//        feedbackService.getFeedbackPaged(1234, 1, 20).then(function(data){$scope.feedbackItems=data;});
-//    }
-//});
 
 
--- webapp/js/lend.js
+++ webapp/js/lend.js
@@ -1,4 +1,8 @@
-var lendApp = angular.module('lend', []);
+var lendApp = angular.module('lend', ['ui.bootstrap.datetimepicker']);
+
+
+var SERVICE_HOST = "http://localhost:8080/"
+//var SERVICE_HOST = "https://ec2-54-173-114-114.compute-1.amazonaws.com/"
 
 lendApp.directive('fileModel', ['$parse', function ($parse) {
     return {
@@ -16,39 +20,92 @@ lendApp.directive('fileModel', ['$parse', function ($parse) {
     };
 }]);
 
-lendApp.service('fileUpload', ['$http', function ($http) {
-    this.uploadFileToUrl = function(file, uploadUrl){
-        console.log(file + ":" + uploadUrl);
+lendApp.service('itemUpload', ['$http', '$log', function ($http, $log) {
+    var getUuid = function() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+        });
+    };
+
+    var prepareItem = function(itemForm, imageUuids) {
+        var itemToUpload = {};
+        itemToUpload.itemName = itemForm.name;
+        itemToUpload.itemDescription = itemForm.description;
+        itemToUpload.pricePerHour = itemForm.price;
+        itemToUpload.images = imageUuids;
+        itemToUpload.quantity = 1;
+        itemToUpload.rentalPeriods = [{
+            "lowerBoundType": "CLOSED",
+            "lowerEndpoint": itemForm.rentalStart.toISOString(),
+            "upperBoundType": "CLOSED",
+            "upperEndpoint": itemForm.rentalEnd.toISOString()
+        }];
+
+        return itemToUpload;
+    };
+
+    this.uploadImage = function(file, uploadUrl){
 
         var fd = new FormData();
-        fd.append('file', file);
+        var imageUuid = getUuid();
+        fd.append('image', file);
+        fd.append('imageUuid', imageUuid);
         $http.post(uploadUrl, fd, {
             transformRequest: angular.identity,
-            headers: {'Content-Type': undefined,
-                      'Authorization': 'bearer c2hhcmUyMDE0LTEyLTE0VDE3OjU3OjMzLjI5MloxNTE4ODgwMjU1'}
+            headers: {
+                'Content-Type': undefined,
+                'Authorization': 'bearer c2hhcmUyMDE0LTEyLTE0VDE3OjU3OjMzLjI5MloxNTE4ODgwMjU1'}
             })
             .success(function(){
+                console.log("image uploaded " + imageUuid);
             })
             .error(function(){
             });
-    }
+
+        return [imageUuid];
+    };
+
+    this.uploadItem = function(item, imageUuids, uploadUrl, $scope) {
+
+
+        var itemToUpload = prepareItem(item, imageUuids);
+
+        $log.info(itemToUpload);
+
+        $http.post(uploadUrl, JSON.stringify(itemToUpload), {
+            transformRequest: angular.identity,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'bearer c2hhcmUyMDE0LTEyLTE0VDE3OjU3OjMzLjI5MloxNTE4ODgwMjU1'}
+        })
+        .success(function(){
+            $log.info("upload successful");
+//                $scope.uploadSuccess = true;
+                $scope.item = {};
+            });
+    };
+
+
 }]);
 
-lendApp.controller('uploadController', ['$scope', '$log', 'fileUpload', function($scope, $log, fileUpload){
+lendApp.controller('uploadController', ['$scope', '$log', 'itemUpload', function($scope, $log, itemUpload){
+    $scope.uploadSuccess = false;
 
-    $scope.uploadFile = function(){
+    $scope.uploadImage = function(){
         var file = $scope.item.image;
         console.log('file is ' + JSON.stringify(file));
-        var uploadUrl = "https://ec2-54-173-114-114.compute-1.amazonaws.com/image?clientId=e7568b2c-2c0f-480e-9e34-08f9a4b807dc";
-        fileUpload.uploadFileToUrl(file, uploadUrl);
+        var uploadUrl = SERVICE_HOST + "image?clientId=e7568b2c-2c0f-480e-9e34-08f9a4b807dc";
+        return itemUpload.uploadImage(file, uploadUrl);
     };
 
     $scope.uploadItem = function(item) {
         // upload file
-        $scope.uploadFile();
+        var uploadedImageKeys = $scope.uploadImage();
 
         // upload the item
-        $log.info(item);
+        var itemUploadUrl = SERVICE_HOST + 'shareitem/7ffc2295-6875-4f40-bc65-827b8fd4535b?clientId=e7568b2c-2c0f-480e-9e34-08f9a4b807dc';
+        itemUpload.uploadItem(item, uploadedImageKeys, itemUploadUrl, $scope);
 
     };
 }]);
